acb257c847741269b4fb0c9ba36a8853023ea9d2
Zack A <boppy@nwcpz.com>
2021-09-11 02:49:10 -0700

    add softbrid theme

---
+++ lua/hl_themes/softbrid.lua
@@ -0,0 +1,35 @@
+local colors = {
+   white = "#ebdbb2",
+   darker_black = "#14161e",
+   black = "#161821", --  nvim bg
+   black2 = "#232634",
+   one_bg = "#2f3346",
+   one_bg2 = "#3a3f56",
+   one_bg3 = "#444a65",
+   grey = "#4e5473",
+   grey_fg = "#575e80",
+   grey_fg2 = "#b9bdcf",
+   light_grey = "#676f97",
+   red = "#b66467",
+   baby_pink = "#fbe6ea",
+   pink = "#e193ab",
+   line = "#2c2f30", -- for lines like vertsplit
+   green = "#8d987e",
+   vibrant_green = "#9cab51",
+   nord_blue = "#8197bf",
+   blue = "#4b6595",
+   yellow = "#dcbb8c",
+   sun = "#fabb6e",
+   purple = "#958793",
+   dark_purple = "#c6b6ee",
+   teal = "#6ec2bf",
+   orange = "#e78a4e",
+   cyan = "#8aa6a5",
+   statusline_bg = "#1f222f",
+   lightbg = "#282c3c",
+   lightbg2 = "#31364a",
+   pmenu_bg = "#8aa6a5",
+   folder_bg = "#8aa6a5",
+}
+
+return colors
---
+++ lua/themes/softbrid-base16.lua
@@ -0,0 +1,19 @@
+return {
+   base00 = "161821",
+   base01 = "3c3836",
+   base02 = "504945",
+   base03 = "665c54",
+   base04 = "bdae93",
+   base05 = "d5c4a1",
+   base06 = "ebdbb2",
+   base07 = "fbf1c7",
+   base08 = "b66467",
+   base09 = "c6b6ee",
+   base0A = "fabb6e",
+   base0B = "8d987e",
+   base0C = "8aa6a5",
+   base0D = "8aa6a5",
+   base0E = "8197bf",
+   base0F = "d3d3c4",
+}
+
